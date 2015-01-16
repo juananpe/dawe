6aa1af164220cc2b9a97304506d514c95143875e
Juanan Pereira <juanan.pereira@ehu.es>
2015-01-16 13:49:59 +0100

    Attempt to add Unit testing automation

---
+++ test/test.js
@@ -0,0 +1,11 @@
+(function(module, test) {
+
+  module("html5 tags unit tests");
+
+  test("Canvas pixels from manual fills", function(assert) {
+	assert.expect(1);
+	assert.ok(true, "Prueba");
+  });
+
+
+})(QUnit.module, QUnit.test);
