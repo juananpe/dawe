caecdc18b6cb324726133cdf8c73c23016ba0487
Juanan Pereira <juanan.pereira@ehu.es>
2016-03-25 18:36:19 +0100

    Add CORS support

--- json/libros.py
+++ json/libros.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask.ext.cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
+@cross_origin()
 def libros():
     with open('libros.json', 'r') as f:
         dat = f.read()
-    
-    return dat
+
+    return (dat)
+#    resp = Response(dat, status=200, mimetype='application/json')
+#    return resp 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(threaded=True)
