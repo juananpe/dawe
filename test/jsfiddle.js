b38e879481fdf903145aba91821e9ecee081b086
Juanan Pereira <juanan.pereira@ehu.eus>
2016-04-11 13:55:09 +0200

    Add a phantomjs script for automating jsfiddle test verification

---
+++ test/jsfiddle.js
@@ -0,0 +1,42 @@
+var page = new WebPage();
+var url = 'http://jsfiddle.net/ancalotoru/tuya1dL9/';
+
+
+//      xpath --> //*[@id="qunit-testresult"]/*/text()
+//     [ passed, total, failed ]
+
+function getElementByXpath(path) {
+	var template = document.createElement("template");
+	template.innerHTML = page.frameContent;
+	// return document.evaluate(path, template, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+	var message = [];
+	var xPathRes = document.evaluate ( path, template, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+
+	var element = xPathRes.iterateNext ();
+	while (element) {
+		if (element.innerHTML != '')
+                        message.push(element.innerHTML);
+		element = xPathRes.iterateNext ()
+	}
+
+	return message;
+
+}
+
+
+page.open(url, function (status) {
+		if ('success' !== status) {
+		console.log("Error");
+		} else {
+		page.switchToFrame("result");
+		setTimeout(function() {
+				// console.log(page.frameContent);
+				// console.log(page.frameContent);
+				var result = getElementByXpath("//*[@id='qunit-testresult']/*");
+				console.log("Passed, total, failed: " + result);
+
+				phantom.exit();
+				},10000);
+		}
+		});
