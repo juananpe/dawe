08bd3d0fa94ce9c7844c4be73ca720bb82e211ba
Juanan Pereira <juanan.pereira@ehu.eus>
2016-05-30 15:32:22 +0200

    Added first test for jquery exercises

---
+++ test/jquery_exercise.js
@@ -0,0 +1,30 @@
+var page = new WebPage();
+var args = require('system').args;
+
+var url = args[1];
+
+// var url = 'http://jsfiddle.net/qv8Pd/'; // 1 sec
+
+
+function getElementByXpath(path) {
+	var template = document.createElement("template");
+	template.innerHTML = page.frameContent;
+	return document.evaluate(path, template, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+
+page.open(url, function (status) {
+		if ('success' !== status) {
+		console.log("Error");
+		} else {
+		page.switchToFrame("result");
+		setTimeout(function() {
+				// console.log(page.frameContent);
+				// console.log(page.frameContent);
+				var result = getElementByXpath("//*[@id='target']");
+				console.log("Passed, total, failed: " + result.style.backgroundColor);
+
+				phantom.exit();
+				},1000);
+		}
+		});
