2c337082a1f5bef562cec6520af7f3b4096c7756
Juanan Pereira <juanan.pereira@ehu.eus>
2016-05-30 17:10:34 +0200

    Check if button is disabled

---
+++ test/jquery_exercise_5.js
@@ -0,0 +1,32 @@
+var page = new WebPage();
+var args = require('system').args;
+
+var url = args[1];
+
+// var url = 'http://jsfiddle.net/qv8Pd/'; // 1 sec
+
+function getElementByXpath(path) {
+	var template = document.createElement("template");
+	template.innerHTML = page.frameContent;
+	return document.evaluate(path, template, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+page.onError =  function(e, trace) {
+	console.log(e);
+};
+
+page.open(url, function (status) {
+		if ('success' !== status) {
+		console.log("Error");
+		} else {
+		page.switchToFrame("result");
+		setTimeout(function() {
+				// console.log(page.frameContent);
+				// console.log(page.frameContent);
+				var result = getElementByXpath("//*[@class='target']");
+				console.log("Disabled:" + result.childNodes[2].disabled);
+
+				phantom.exit();
+				},1000);
+		}
+		});
